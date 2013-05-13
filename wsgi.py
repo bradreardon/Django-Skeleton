d7b96205410f177b8f7b23bf75137a2e0b52be65
Brad Reardon <brad.jay.reardon@gmail.com>
2013-05-12 21:19:56 -0400

    Updated WSGI script to use configuration scheme in this skeleton.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.base.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
